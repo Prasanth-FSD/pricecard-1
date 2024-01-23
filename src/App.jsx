456887f445db6d1418fc80b3bc924eeaf68be077
PRASANTH D <chinnakutty1470@gmail.com>
2024-01-23 14:29:17 +0530

    hello

---
+++ src/App.jsx
@@ -0,0 +1,28 @@
+import './App.css';
+import './ProductCard';
+import ProductCard from './ProductCard';
+import '../node_modules/bootstrap/dist/css/bootstrap.min.css'
+import '../node_modules/font-awesome/css/font-awesome.min.css'
+import ProductCard2 from './ProductCard2';
+import ProductCard3 from './ProductCard3';
+function App() {
+  return (
+    <div>
+      <div className="container-fluid">
+        <div className="row">
+          <div className="col-sm-4 col-lg-6 col-sm-12 col-xl-4 col-xxl-4 col-md-6">
+            <ProductCard plan="FREE" month="$0/month"/>
+          </div>
+          <div className="col-sm-4 col-lg-6 col-sm-12 col-xl-4 col-xxl-4 col-md-6">
+            <ProductCard2 plan="PLUS" month="$9/month"/>
+          </div>
+          <div className="col-sm-4 col-lg-6 col-sm-12 col-xl-4 col-xxl-4 col-md-6">
+            <ProductCard3 plan="PRO" month="$49/month"/>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default App;
